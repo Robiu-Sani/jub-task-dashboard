be2b859c55c941646c3e2291f37322e932ec0cfe
Robiu-Sani <hafazrobiussani@gmail.com>
2024-10-18 18:03:39 +0600

    top navbar is done

--- src/Component/DashboardPages/DashboardRoot.jsx
+++ src/Component/DashboardPages/DashboardRoot.jsx
@@ -1,5 +1,8 @@
 import { Outlet } from "react-router-dom";
 import DashboardNav from "./DashboardNav";
+import { VscThreeBars } from "react-icons/vsc";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
 
 export default function DashboardRoot() {
   return (
@@ -8,7 +11,25 @@ export default function DashboardRoot() {
         <DashboardNav />
       </div>
       <div className="w-[calc(100%-40px)] mr-5 ">
-        <div className="w-full h-[65px] flex justify-between mb-2  border-b items-center"></div>
+        <div className="w-full h-[65px] flex justify-between mb-2  border-b items-center">
+          <VscThreeBars className="p-1 cursor-pointer block md:hidden" />
+          <div className="hidded md:flex gap-3">
+            <p className="text-gray-500">Invoices</p>
+            <p className="text-gray-500">/</p>
+            <p className="text-gray-700">Create Invoices</p>
+          </div>
+          <div className="flex justify-center items-center gap-3">
+            <div className="relative flex justify-center items-center ">
+              <IoSearch className="md:absolute md:left-2 text-blue-600 " />
+              <input
+                type="search"
+                placeholder="Search here"
+                className="px-3 pl-7 hidden md:block w-[300px] py-1 rounded-md text-blue-600 placeholder:text-blue-400 bg-blue-100"
+              />
+            </div>
+            <IoMdNotificationsOutline className="text-blue-600" />
+          </div>
+        </div>
         <div className="w-full h-[calc(100vh-65px)] overflow-y-auto noneScroll">
           <Outlet />
         </div>
